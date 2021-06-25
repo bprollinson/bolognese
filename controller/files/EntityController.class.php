7386f4950bf0228965c0e7822ee98b31b77a8f81
bprollinson <brendan.rollinson@gmail.com>
2021-06-25 00:03:08 -0400

    Adding count endpoint

--- controller/files/EntityController.class.php
+++ controller/files/EntityController.class.php
@@ -5,6 +5,31 @@ require_once('MethodInvoked.class.php');
 
 class EntityController
 {
+    public function countEntities()
+    {
+        $hostIP = gethostbyname('database_client');
+        $port = 50002;
+        $connection = new ClientSocketConnection($hostIP, $port);
+        if (!$connection->open())
+        {
+            return new MethodInvoked('entity_fetched', 0);
+        }
+
+        $requestParameters = [
+            'type' => 'select_scalar',
+            'query' => 'SELECT COUNT(1) FROM entity'
+        ];
+        $message = json_encode($requestParameters);
+        $connection->write($message);
+
+        $response = $connection->read();
+        $connection->close();
+
+        $responseJson = json_decode($response, true);
+
+        return new MethodInvoked('entites_counted', $responseJson['result']);
+    }
+
     public function getEntity()
     {
         $hostIP = gethostbyname('database_client');
